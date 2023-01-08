411cba68c8265eb5a51cf457685d04079dd71ba6
Tanner Swett <tannerswett@gmail.com>
2023-01-07 20:54:20 -0500

    Refactor main.c a bit

--- main.c
+++ main.c
@@ -14,6 +14,8 @@
 
 #include "heap.h"
 
+
+
 // Command parsing and processing:
 
 // Read a command from the user and run it
@@ -24,13 +26,31 @@ int try_parse_int(char *input, int *result);
 // Individual commands:
 
 // Get and print the value of a field in a cell
-void cmd_getfield(int field, int index);
+void cmd_getfield(int field, char *command_name);
 // Set the value of a field in a cell
-void cmd_setfield(int field, int index, int value);
-#define COMMAND_GETCAR 0
-#define COMMAND_GETCDR 1
-#define COMMAND_SETCAR 2
-#define COMMAND_SETCDR 3
+void cmd_setfield(int field, char *command_name);
+
+// Error messages:
+
+// Print "Unrecognized command: %s"
+void unknown_command(char *command_name);
+// Print "Too few arguments to %s"
+void too_few_arguments(char *command_name);
+// Print "Too many arguments to %s"
+void too_many_arguments(char *command_name);
+// Print "Invalid number: %s"
+void invalid_number(char *word);
+// Print "Index out of range: %d"
+void index_out_of_range(int index);
+
+// Argument parsing using strtok:
+
+// Try to get an int argument; return 0 on failure
+int get_int_argument_strtok(char *command_name, int *result);
+// Assert that there are no more arguments; return 0 if there is one
+int no_more_arguments_strtok(char *command_name);
+
+
 
 // Command parsing and processing:
 
@@ -42,106 +62,114 @@ int main(int argc, char **argv) {
 
 void process_command(void) {
     char command[1024];
-    char *word;
-    char *command_name;
-    int command_num;
 
     fprintf(stderr, "> ");
     if (!fgets(command, sizeof(command), stdin))
         return;
 
-    command_name = strtok(command, " \n");
+    char *command_name = strtok(command, " \n");
 
     if (!command_name)
         return;
 
     if (strcmp(command_name, "getcar") == 0)
-        command_num = COMMAND_GETCAR;
+        cmd_getfield(FIELD_CAR, command_name);
     else if (strcmp(command_name, "getcdr") == 0)
-        command_num = COMMAND_GETCDR;
+        cmd_getfield(FIELD_CDR, command_name);
     else if (strcmp(command_name, "setcar") == 0)
-        command_num = COMMAND_SETCAR;
+        cmd_setfield(FIELD_CAR, command_name);
     else if (strcmp(command_name, "setcdr") == 0)
-        command_num = COMMAND_SETCDR;
+        cmd_setfield(FIELD_CDR, command_name);
     else
-        goto unknown_command;
+        unknown_command(command_name);
 
-    switch (command_num) {
-        case COMMAND_GETCAR:
-        case COMMAND_GETCDR: {
-            int index;
-
-            word = strtok(NULL, " \n");
-            if (!word) goto too_few_arguments;
-            if (!try_parse_int(word, &index)) goto invalid_number;
-
-            word = strtok(NULL, " \n");
-            if (word) goto too_many_arguments;
-
-            switch (command_num) {
-                case COMMAND_GETCAR:
-                    cmd_getfield(FIELD_CAR, index);
-                    break;
-                case COMMAND_GETCDR:
-                    cmd_getfield(FIELD_CDR, index);
-                    break;
-            }
-
-            break;
-        }
-
-        case COMMAND_SETCAR:
-        case COMMAND_SETCDR: {
-            int index, value;
-
-            word = strtok(NULL, " \n");
-            if (!word) goto too_few_arguments;
-            if (!try_parse_int(word, &index)) goto invalid_number;
-
-            word = strtok(NULL, " \n");
-            if (!word) goto too_few_arguments;
-            if (!try_parse_int(word, &value)) goto invalid_number;
-
-            word = strtok(NULL, " \n");
-            if (word) goto too_many_arguments;
-
-            switch (command_num) {
-                case COMMAND_SETCAR:
-                    cmd_setfield(FIELD_CAR, index, value);
-                    break;
-                case COMMAND_SETCDR:
-                    cmd_setfield(FIELD_CDR, index, value);
-                    break;
-            }
-
-            break;
-        }
-    }
-
-    return;
-
-    unknown_command:
-    fprintf(stderr, "Unrecognized command: %s\n", command_name);
-    return;
-
-    too_few_arguments:
-    fprintf(stderr, "Too few arguments to %s\n", command_name);
-    return;
-
-    too_many_arguments:
-    fprintf(stderr, "Too many arguments to %s\n", command_name);
-    return;
-
-    invalid_number:
-    fprintf(stderr, "Invalid number: %s\n", word);
     return;
 }
 
-int try_parse_int(char *input, int *result) {
-    char *remainder;
-    int strtoul_result = strtoul(input, &remainder, 10);
 
-    if (remainder == input) {
+
+// Individual commands:
+
+// command_name: read-only pointer to a string
+void cmd_getfield(int field, char *command_name) {
+    int index;
+
+    if (!get_int_argument_strtok(command_name, &index)) return;
+    if (!no_more_arguments_strtok(command_name)) return;
+
+    if (index < 0 || index >= HEAP_SIZE) {
+        index_out_of_range(index);
+        return;
+    }
+
+    int result = getfield(field, index);
+
+    printf("%d\n", result);
+}
+
+// command_name: read-only pointer to a string
+void cmd_setfield(int field, char *command_name) {
+    int index, value;
+
+    if (!get_int_argument_strtok(command_name, &index)) return;
+    if (!get_int_argument_strtok(command_name, &value)) return;
+    if (!no_more_arguments_strtok(command_name)) return;
+
+    if (index < 0 || index >= HEAP_SIZE) {
+        index_out_of_range(index);
+        return;
+    }
+
+    setfield(field, index, value);
+}
+
+
+
+// Error messages:
+
+// command_name: read-only pointer to a string
+void unknown_command(char *command_name) {
+    fprintf(stderr, "Unrecognized command: %s\n", command_name);
+}
+
+// command_name: read-only pointer to a string
+void too_few_arguments(char *command_name) {
+    fprintf(stderr, "Too few arguments to %s\n", command_name);
+}
+
+// command_name: read-only pointer to a string
+void too_many_arguments(char *command_name) {
+    fprintf(stderr, "Too many arguments to %s\n", command_name);
+}
+
+// word: read-only pointer to a string
+void invalid_number(char *word) {
+    fprintf(stderr, "Invalid number: %s\n", word);
+}
+
+void index_out_of_range(int index) {
+    fprintf(stderr, "Index out of range: %d\n", index);
+}
+
+
+
+// Argument parsing using strtok:
+
+// command_name: read-only pointer to a string
+// result: output pointer to an int
+int get_int_argument_strtok(char *command_name, int *result) {
+    char *word = strtok(NULL, " \n");
+
+    if (!word) {
+        too_few_arguments(command_name);
+        return 0;
+    }
+
+    char *remainder;
+    int strtoul_result = strtoul(word, &remainder, 10);
+
+    if (remainder == word) {
+        invalid_number(word);
         return 0;
     } else {
         *result = strtoul_result;
@@ -149,26 +177,14 @@ int try_parse_int(char *input, int *result) {
     }
 }
 
-// Individual commands:
+// command_name: read-only pointer to a string
+int no_more_arguments_strtok(char *command_name) {
+    char *word = strtok(NULL, " \n");
 
-void cmd_getfield(int field, int index) {
-    int result;
-
-    if (index < 0 || index >= HEAP_SIZE) {
-        fprintf(stderr, "Index out of range: %d\n", index);
-        return;
+    if (word) {
+        too_many_arguments(command_name);
+        return 0;
+    } else {
+        return 1;
     }
-
-    result = getfield(field, index);
-
-    printf("%d\n", result);
-}
-
-void cmd_setfield(int field, int index, int value) {
-    if (index < 0 || index >= HEAP_SIZE) {
-        fprintf(stderr, "Index out of range: %d\n", index);
-        return;
-    }
-
-    setfield(field, index, value);
 }
